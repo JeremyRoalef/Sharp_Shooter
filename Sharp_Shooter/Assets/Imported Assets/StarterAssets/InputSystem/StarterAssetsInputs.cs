1ddb3aaa14b3d10ea0498caa8a32ea6baa4fc833
JeremyRoalef <roalef944@morrisville.edu>
2025-04-12 16:45:22 -0400

    6.7 Shoot Input
    Very weird architecture for handling button pressing.
    
    Boolean variables in input class can be listened to in other classes, and the variable will be reset to false when the value is true in the class that is listening to the bool variable.

--- Sharp_Shooter/Assets/Imported Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Sharp_Shooter/Assets/Imported Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 #if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
@@ -12,6 +13,7 @@ namespace StarterAssets
 		public Vector2 look;
 		public bool jump;
 		public bool sprint;
+		public bool shoot;
 
 		[Header("Movement Settings")]
 		public bool analogMovement;
@@ -43,10 +45,15 @@ namespace StarterAssets
 		{
 			SprintInput(value.isPressed);
 		}
+
+		public void OnShoot(InputValue value)
+		{
+			ShootInput(value.isPressed);
+		}
 #endif
 
 
-		public void MoveInput(Vector2 newMoveDirection)
+        public void MoveInput(Vector2 newMoveDirection)
 		{
 			move = newMoveDirection;
 		} 
@@ -65,8 +72,12 @@ namespace StarterAssets
 		{
 			sprint = newSprintState;
 		}
-		
-		private void OnApplicationFocus(bool hasFocus)
+        public void ShootInput(bool newShootState)
+        {
+			shoot = newShootState;
+        }
+
+        private void OnApplicationFocus(bool hasFocus)
 		{
 			SetCursorState(cursorLocked);
 		}
--- Sharp_Shooter/Assets/Scripts/Weapon.cs
+++ Sharp_Shooter/Assets/Scripts/Weapon.cs
@@ -1,9 +1,18 @@
+using StarterAssets;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    StarterAssetsInputs starterAssetsInputs;
+
     [SerializeField]
     float maxRayDistance;
+
+    private void Awake()
+    {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+    }
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,13 +23,18 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //RaycastHit interacts with rigidbodys and colliders
-        RaycastHit hit;
-
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        if (starterAssetsInputs.shoot)
         {
-            //If hit has output, this will run
-            Debug.Log(hit.collider.name);
+            //RaycastHit interacts with rigidbodys and colliders
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+            {
+                //If hit has output, this will run
+                Debug.Log(hit.collider.name);
+            }
+
+            starterAssetsInputs.ShootInput(false);
         }
     }
 }
