23ededc9a87bc4d63516ef6396ec21ad759c4e3f
JeremyRoalef <roalef944@morrisville.edu>
2025-04-13 12:02:04 -0400

    6.34 Player Shield UI
    Not the best setup for scalability, but the point gets across all the same.
    
    Added UI for player health/shield and adjusted the amount accordingly

--- Sharp_Shooter/Assets/Scripts/Player/PlayerHealth.cs
+++ Sharp_Shooter/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System;
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -16,19 +17,41 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField]
     Transform weaponCamera;
 
+    [SerializeField]
+    Image[] shieldBars;
+
     int gameOverVirtualCameraPriority = 20;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        AdjustShieldUI();
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+
+        AdjustShieldUI();
+
         if (currentHealth <= 0)
         {
             Die();
+        }
+    }
+
+    private void AdjustShieldUI()
+    {
+        for (int i = 0; i < shieldBars.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                shieldBars[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                shieldBars[i].gameObject.SetActive(false);
+            }
         }
     }
 
