f5d25ca2204440e8b144f5ea2e2dd26b1fe2c0ef
JeremyRoalef <roalef944@morrisville.edu>
2025-04-13 00:47:04 -0400

    6.27 Ammo Pickup
    New pickup: Ammo. Follows same architecture as other pickups

--- Sharp_Shooter/Assets/Scripts/Pickups/AmmoPickup.cs
+++ Sharp_Shooter/Assets/Scripts/Pickups/AmmoPickup.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class AmmoPickup : Pickup
 {
+    [SerializeField]
+    int ammoAmount = 24;
 
     protected override void OnPickup(ActiveWeapon activeWeapon)
     {
-
+        activeWeapon.AdjustAmmo(ammoAmount);
     }
 }
